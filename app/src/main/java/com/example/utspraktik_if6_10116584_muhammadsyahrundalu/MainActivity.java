90e3ef481756ac6278bb832da9479648149f2252
syahrundalu <dalu.syahrun@gmail.com>
2023-05-25 21:33:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10116584_muhammadsyahrundalu/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.utspraktik_if6_10116584_muhammadsyahrundalu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/*
+    NIM : 10116584
+    NAMA : Muhammad Syahrun Dalu
+*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
